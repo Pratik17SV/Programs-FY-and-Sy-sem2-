f2385cfadd510d7cf915133ed7f358a0419f6564
Pratik17SV <pratik.dg@somaiya.edu>
2025-01-05 21:26:33 +0530

    created external file for JS.

---
+++ JavsScript/SlidshowJS/image.js
@@ -0,0 +1,24 @@
+var myimage = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg"];
+var currentimage = 0;
+document.getElementById('next').onclick= nextphoto;
+function nextphoto(){
+    
+    if(currentimage >= (myimage.length-1)){
+        currentimage=0;
+    }else{
+        currentimage++;
+    }
+
+    document.getElementById('myimage').src = myimage[currentimage];
+}
+
+document.getElementById('Previous').onclick=Previous;
+function Previous(){
+    if(currentimage <= 0){
+        currentimage=myimage.length-1;
+    }
+    else{
+        currentimage--;
+    }
+    document.getElementById('myimage').src = myimage[currentimage];
+}
