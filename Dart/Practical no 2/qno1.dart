16816308fe60120ddd58efdebf86a8f8a971645c
PratikSV <pratikgaikwad7738@gmail.com>
2025-03-03 19:52:44 +0530

    dart prac program compleated.

--- Dart/Practical no 2/qno1.dart
+++ Dart/Practical no 2/qno1.dart
@@ -1,93 +1,87 @@
-//Create a dart program to demonstrate hybrid inheritance between the classes of Student, 
-//BCAStudent, CommerceStudent, InternshipCandidate under both the streams. 
-//Student: name, roll no, institute name (static variable). 
-//Display fee information, the payment status and function to make payment. 
-//Subject selection method in BCA and Commerce. 
+//Create a dart program to demonstrate hybrid inheritance between the classes of Student,
+//BCAStudent, CommerceStudent, InternshipCandidate under both the streams.
+//Student: name, roll no, institute name (static variable).
+//Display fee information, the payment status and function to make payment.
+//Subject selection method in BCA and Commerce.
 //IntershipCandidate: Company, duration, stipend.
 
-class Student 
-{
+class Student {
   static String instituteName = "S.K Somaiya";
   String name;
   int rollNo;
   Student(this.name, this.rollNo);
-  void displayFeeInfo() 
-  {
+  void displayFeeInfo() {
     print("Fee info for $name (Rollno: $rollNo) at $instituteName");
   }
-  void paymentStatus() 
-  {
+
+  void paymentStatus() {
     print("Payment status for $name: Pending");
   }
-  void makePayment() 
-  {
+
+  void makePayment() {
     print("Payment made for $name.");
   }
 }
 
-class BCAStudent extends Student 
-{
+class BCAStudent extends Student {
   BCAStudent(String name, int rollNo) : super(name, rollNo);
-  void selectSubjects() 
-  {
+  void selectSubjects() {
     print("$name has selected BCA subjects.");
   }
 }
 
-class CommerceStudent extends Student 
-{
+class CommerceStudent extends Student {
   CommerceStudent(String name, int rollNo) : super(name, rollNo);
-  void selectSubjects() 
-  {
+  void selectSubjects() {
     print("$name has selected Commerce subjects.");
   }
 }
-mixin InternshipCandidate 
-{
+
+mixin InternshipCandidate {
   String company = "";
   String duration = "";
   double stipend = 0.0;
-  void displayInternshipDetails() 
-  {
-    print("Internship at $company for $duration with stipend of \$${stipend}");
+  void displayInternshipDetails() {
+    print(
+        "Internship at $company for $duration with stipend of  Rs:\${stipend}");
   }
 }
 
-class BCAInternshipCandidate extends BCAStudent with InternshipCandidate 
-{
-  BCAInternshipCandidate(String name, int rollNo, String company, String duration, double stipend) : super(name, rollNo) 
-  {
+class BCAInternshipCandidate extends BCAStudent with InternshipCandidate {
+  BCAInternshipCandidate(
+      String name, int rollNo, String company, String duration, double stipend)
+      : super(name, rollNo) {
     this.company = company;
     this.duration = duration;
     this.stipend = stipend;
   }
 }
 
-class CommerceInternshipCandidate extends CommerceStudent with InternshipCandidate 
-{
-  CommerceInternshipCandidate(String name, int rollNo, String company, String duration, double stipend) : super(name, rollNo) 
-  {
+class CommerceInternshipCandidate extends CommerceStudent
+    with InternshipCandidate {
+  CommerceInternshipCandidate(
+      String name, int rollNo, String company, String duration, double stipend)
+      : super(name, rollNo) {
     this.company = company;
     this.duration = duration;
     this.stipend = stipend;
   }
 }
 
-void main() 
-{
-  BCAInternshipCandidate bcaIntern = BCAInternshipCandidate("Aditi", 101, "Microsoft", "6 months", 5000);
+void main() {
+  BCAInternshipCandidate bcaIntern =
+      BCAInternshipCandidate("Pratik", 019, "Tesla", "10 months", 19000);
   bcaIntern.displayFeeInfo();
   bcaIntern.paymentStatus();
   bcaIntern.makePayment();
   bcaIntern.selectSubjects();
   bcaIntern.displayInternshipDetails();
-  print("---------------------");
-  CommerceInternshipCandidate commerceIntern = CommerceInternshipCandidate("Paurnima", 102, "Finance Inc", "9 months", 2500);
+  print("=========================");
+  CommerceInternshipCandidate commerceIntern =
+      CommerceInternshipCandidate("Vinayak", 011, "Google", "10 months", 25000);
   commerceIntern.displayFeeInfo();
   commerceIntern.paymentStatus();
   commerceIntern.makePayment();
   commerceIntern.selectSubjects();
   commerceIntern.displayInternshipDetails();
 }
-
-
